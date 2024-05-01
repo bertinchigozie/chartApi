1667a404a500a0cdf94286b9c9e035b5deca7852
Okeke Chigozie <81691598+bertinchigozie@users.noreply.github.com>
2024-05-01 17:46:18 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const dataRouter = require("./src/components/routes/dataRoute");
+const cors = require("cors");
 
 const app = express();
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With,Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+//   next();
+// });
 app.use("/", dataRouter);
 
 module.exports = app;
